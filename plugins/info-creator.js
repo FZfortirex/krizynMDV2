da4b530a88cc71493b1d93a3e78e75d1445fbd6b
FZfortirex <99706613+FZfortirex@users.noreply.github.com>
2022-07-15 13:15:02 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn }) => {
-conn.sendContact(m.chat, numberowner, nameowner, m)
+conn.sendContact(m.chat, 6285728581059, Burhanuddin Zain, m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
